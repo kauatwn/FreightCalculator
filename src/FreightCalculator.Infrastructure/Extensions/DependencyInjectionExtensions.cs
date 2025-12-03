97fbfb9cd2a0e0ba5542a83a3c1534299eb1b94a
kauatwn <kaualopescontato@gmail.com>
2025-12-03 19:46:58 -0300

    feat(infra): add configuration extensions and validation packages

--- src/FreightCalculator.Infrastructure/Extensions/DependencyInjectionExtensions.cs
+++ src/FreightCalculator.Infrastructure/Extensions/DependencyInjectionExtensions.cs
@@ -1,14 +1,32 @@
-﻿using FreightCalculator.Domain.Enums;
+﻿using FreightCalculator.Domain.Configuration;
+using FreightCalculator.Domain.Enums;
 using FreightCalculator.Domain.Interfaces;
 using FreightCalculator.Domain.Services.Shipping;
 using FreightCalculator.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FreightCalculator.Infrastructure.Extensions;
 
 public static class DependencyInjectionExtensions
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        AddConfiguration(services, configuration);
+        AddServices(services);
+
+        return services;
+    }
+
+    private static void AddConfiguration(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddOptions<ShippingSettings>()
+            .Bind(configuration.GetSection(ShippingSettings.SectionName))
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+    }
+
+    private static void AddServices(IServiceCollection services)
     {
         services.AddKeyedScoped<IShippingService, StandardShippingService>(ShippingMethod.Standard);
         services.AddKeyedScoped<IShippingService, ExpressShippingService>(ShippingMethod.Express);
@@ -17,7 +35,5 @@ public static class DependencyInjectionExtensions
         {
             return new ShippingServiceFactory(method => provider.GetRequiredKeyedService<IShippingService>(method));
         });
-
-        return services;
     }
 }
